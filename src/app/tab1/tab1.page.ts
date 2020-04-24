df6689f1fe98196f7fa904f5b378f930269b1b37
Debbie Hillyar <debbiehillyar1@gmail.com>
2020-04-24 20:51:08 +0100

    unit109 CR

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,10 @@
+import { Message } from './../models/message';
 import { Component } from '@angular/core';
+
+import { DataService } from '../services/data.service';
+
+import { ShareService } from '../services/share.service';
+import { Friend } from '../models/friend';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +12,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  
+  myFriends: Friend[] = [];
+  allMessages : Message[] = [];
+  friendFilter = "Everyone";
+  constructor(private data : DataService, public shared: ShareService) {
+     // get data
+     data.getAllMessages().subscribe(list => {
+      console.log("messages", list);
+      this.allMessages = list.filter(m => m.to == "Everyone" 
+      || m.to == shared.userName 
+      || m.from == shared.userName);
+    });
+    data.getAllFriends().subscribe(list => {
+      this.myFriends = list.filter(f => f.belongsTo == shared.userName);
+    });
 
-  constructor() {}
+  }
+  getMessagesToDisplay(){
+    if(this.friendFilter == "Everyone") return this.allMessages;
 
+    return this.allMessages.filter(m => m.from == this.friendFilter 
+      ||( m.from == this.shared.userName && m.to == this.friendFilter) );
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
+import { ShareService } from './../services/share.service';
+import { Message } from './../models/message';
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Friend } from '../models/friend';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  post: Message = new Message();  
+  myFriends: Friend[] = [];
 
-  constructor() {}
+  constructor(private sharedSrvc: ShareService, private dataSrvc: DataService) {
+    this.dataSrvc.getAllFriends().subscribe(list => {
+      console.log("all friends", list);
+      this.myFriends = [];
+      this.myFriends = list.filter(f => f.belongsTo == this.sharedSrvc.userName);// filter function
+    });
+  }
+  sendMessage() {
+    this.post.from = this.sharedSrvc.userName;
+    this.dataSrvc.saveMessage(this.post);
+
+    this.post = new Message();
+  }
+
 
 }
